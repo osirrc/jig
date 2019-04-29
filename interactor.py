f07c03ebcd141128576efca7584d81ba1dfda80c
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-04-29 19:39:23 -0400

    Add requirements.txt... (#63)
    * Add requirements.txt...
    
    and also some minor formatting
    
    * Add docs

--- interactor.py
+++ interactor.py
@@ -1,15 +1,15 @@
 import json
-import os
 import sys
+
 
 class Interactor:
 
     def __init__(self, interactor_config=None):
         self.config = interactor_config
-    
+
     def set_config(self, interactor_config):
         self.config = interactor_config
-    
+
     def interact(self, client, generate_save_tag):
         """
         Starts a container from a saved image that has already been initialized/indexed.
@@ -23,7 +23,7 @@ class Interactor:
         exists = len(client.images.list(filters={"reference": "{}:{}".format(self.config.repo, save_tag)})) != 0
         if not exists:
             sys.exit("Must prepare image first...")
-        
+
         interact_args = {
             "opts": {key: value for (key, value) in map(lambda x: x.split("="), self.config.opts)}
         }
@@ -32,11 +32,11 @@ class Interactor:
         # create with sh command to override any default command
         container = client.containers.create("{}:{}".format(self.config.repo, save_tag), command="sh", tty=True)
         container.start()
-        
+
         print("Running interact script in container...")
-        log = container.exec_run("sh -c '/interact --json {}'".format(json.dumps(json.dumps(interact_args))), 
-                                stdout=True, stderr=True, stream=True)
-        
+        log = container.exec_run("sh -c '/interact --json {}'".format(json.dumps(json.dumps(interact_args))),
+                                 stdout=True, stderr=True, stream=True)
+
         print("Logs for interact in container with ID {}...".format(container.id))
         for line in log[1]:
             print(str(line.decode('utf-8')), end="")
@@ -47,7 +47,7 @@ class Interactor:
             print("Exiting...")
             print("Don't forget to stop and remove the container after you are done!")
             return
-        
+
         wait = input("Press ENTER to stop and remove container")
         print("Stopping container {}...".format(container.id))
         container.stop()

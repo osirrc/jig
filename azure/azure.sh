d778bda11b6e6b98bd650ee8ab4bfb221fc8ff4c
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-06-17 10:25:13 -0400

    Update Azure script

--- azure/azure.sh
+++ azure/azure.sh
@@ -61,51 +61,51 @@ while [[ "$#" -gt 0 ]]; do
     shift;
 done
 
-# Login to Azure
-az login
-
-# Set the subscription
-az account set --subscription ${SUBSCRIPTION}
-
-# Create the VM
-az vm create \
-    --resource-group ${RESOURCE_GROUP} \
-    --name ${VM_NAME} \
-    --image UbuntuLTS \
-    --size ${VM_SIZE} \
-    --attach-data-disks ${DISK_NAME} \
-    --admin-username jig \
-    --ssh-key-value ${SSH_PUBKEY_PATH}
+## Login to Azure
+#az login
+#
+## Set the subscription
+#az account set --subscription ${SUBSCRIPTION}
+#
+## Create the VM
+#az vm create \
+#    --resource-group ${RESOURCE_GROUP} \
+#    --name ${VM_NAME} \
+#    --image UbuntuLTS \
+#    --size ${VM_SIZE} \
+#    --attach-data-disks ${DISK_NAME} \
+#    --admin-username jig \
+#    --ssh-key-value ${SSH_PUBKEY_PATH}
 
 # Get the IP address of the VM
 IP_ADDRESS=$(az vm list-ip-addresses --resource-group ${RESOURCE_GROUP} --name ${VM_NAME} --query "[].virtualMachine.network.publicIpAddresses[].ipAddress[]" -o tsv)
 
-# Copy over Docker daemon.json file
-scp -o StrictHostKeyChecking=accept-new daemon.json jig@${IP_ADDRESS}:/tmp
-
-# Setup jig
-ssh jig@${IP_ADDRESS} << EOF
-
-    mkdir collections
-    sudo mount /dev/sdc1 collections
-
-    sudo apt update
-    sudo apt install -y build-essential docker.io git python3 virtualenv
-
-    sudo addgroup jig docker
-    sudo cp /tmp/daemon.json /etc/docker
-    sudo systemctl restart docker.service
-
-    git clone https://github.com/osirrc2019/jig.git; cd jig
-
-    virtualenv -p /usr/bin/python3 venv
-
-    source venv/bin/activate
-    pip install -r requirements.txt
-
-    git clone https://github.com/usnistgov/trec_eval.git && make -C trec_eval
-
-EOF
+## Copy over Docker daemon.json file
+#scp -o StrictHostKeyChecking=accept-new daemon.json jig@${IP_ADDRESS}:/tmp
+#
+## Setup jig
+#ssh jig@${IP_ADDRESS} << EOF
+#
+#    mkdir collections
+#    sudo mount /dev/sdc1 collections
+#
+#    sudo apt update
+#    sudo apt install -y build-essential docker.io git python3 virtualenv
+#
+#    sudo addgroup jig docker
+#    sudo cp /tmp/daemon.json /etc/docker
+#    sudo systemctl restart docker.service
+#
+#    git clone https://github.com/osirrc2019/jig.git; cd jig
+#
+#    virtualenv -p /usr/bin/python3 venv
+#
+#    source venv/bin/activate
+#    pip install -r requirements.txt
+#
+#    git clone https://github.com/usnistgov/trec_eval.git && make -C trec_eval
+#
+#EOF
 
 # The collection name, path, and format
 COLLECTION_NAME=$(cat ${RUN_FILE} | jq -r ".collection.name")
@@ -120,7 +120,7 @@ TOPIC_PATH=$(cat ${RUN_FILE} | jq -r ".topic.path")
 QRELS_PATH=$(cat ${RUN_FILE} | jq -r ".qrels.path")
 
 # The number of images
-NUM_IMAGES=$(cat ${RUN_FILE} | jq -r '.images | length')
+NUM_IMAGES=$(cat ${RUN_FILE} | jq -r ".images | length")
 
 for i in $(seq 0 $((${NUM_IMAGES} - 1))); do
 
@@ -130,21 +130,27 @@ for i in $(seq 0 $((${NUM_IMAGES} - 1))); do
     PREPARE=${PREPARE/"[COLLECTION_PATH]"/${COLLECTION_PATH}}
     PREPARE=${PREPARE/"[COLLECTION_FORMAT]"/${COLLECTION_FORMAT}}
 
-    # Get the search command and substitute in variables
-    SEARCH=$(cat ${RUN_FILE} | jq -r ".images[$i].command.search")
-    SEARCH=${SEARCH/"[COLLECTION_NAME]"/${COLLECTION_NAME}}
-    SEARCH=${SEARCH/"[TOPIC_PATH]"/${TOPIC_PATH}}
-    SEARCH=${SEARCH/"[QRELS_PATH]"/${QRELS_PATH}}
-    SEARCH=${SEARCH/"[OUTPUT]"/${OUTPUT}}
-
     echo ${PREPARE}
-    ssh jig@${IP_ADDRESS} "cd jig && source venv/bin/activate && eval ${PREPARE}"
+    ssh jig@${IP_ADDRESS} "cd jig && source venv/bin/activate && ${PREPARE}"
 
-    echo ${SEARCH}
-    ssh jig@${IP_ADDRESS} "cd jig && source venv/bin/activate && eval ${SEARCH}"
+    NUM_SEARCHES=$(cat ${RUN_FILE} | jq -r ".images[$i].command.search | length")
+
+    for j in $(seq 0 $((${NUM_SEARCHES} - 1))); do
+
+         # Get the search command and substitute in variables
+        SEARCH=$(cat ${RUN_FILE} | jq -r ".images[$i].command.search[$j]")
+        SEARCH=${SEARCH/"[COLLECTION_NAME]"/${COLLECTION_NAME}}
+        SEARCH=${SEARCH/"[TOPIC_PATH]"/${TOPIC_PATH}}
+        SEARCH=${SEARCH/"[QRELS_PATH]"/${QRELS_PATH}}
+        SEARCH=${SEARCH/"[OUTPUT]"/${OUTPUT}}
+
+        echo ${SEARCH}
+        ssh jig@${IP_ADDRESS} "cd jig && source venv/bin/activate && ${SEARCH}"
+
+    done
 
     scp -r jig@${IP_ADDRESS}:${OUTPUT} .
 
 done
 
-az vm delete --resource-group ${RESOURCE_GROUP} --name ${VM_NAME}
+#az vm delete --resource-group ${RESOURCE_GROUP} --name ${VM_NAME}
--- run.py
+++ run.py
@@ -24,9 +24,8 @@ if __name__ == "__main__":
     parser_prepare.set_defaults(run=manager.prepare)
     parser_prepare.add_argument("--repo", required=True, type=str, help="the image repo (i.e., osirrc2019/anserini)")
     parser_prepare.add_argument("--tag", default="latest", type=str, help="the image tag (i.e., latest)")
-    parser_prepare.add_argument("--save_to_snapshot", default="save", type=str,
-                                help="used to determine the tag of the snapshotted image after indexing")
-    parser_prepare.add_argument("--collections", required=True, nargs="+", help="the name of the collection")
+    parser_prepare.add_argument("--save_to_snapshot", default="save", type=str, help="used to determine the tag of the snapshotted image after indexing")
+    parser_prepare.add_argument("--collections", nargs="+", help="the name of the collection")
     parser_prepare.add_argument("--opts", nargs="+", default="", type=str, help="the args passed to the index script")
 
     # Specific to train
@@ -34,19 +33,14 @@ if __name__ == "__main__":
     trainer_prepare.set_defaults(run=manager.train)
     trainer_prepare.add_argument("--repo", required=True, type=str, help="the image repo (i.e., albep/nvsm)")
     trainer_prepare.add_argument("--tag", default="latest", type=str, help="the image tag (i.e., latest)")
-    trainer_prepare.add_argument("--load_from_snapshot", default="save", type=str,
-                                 help="used to determine the tag of the snapshotted image to train from")
+    trainer_prepare.add_argument("--load_from_snapshot", default="save", type=str, help="used to determine the tag of the snapshotted image to train from")
     trainer_prepare.add_argument("--topic", required=True, type=str, help="the topic file for search")
-    trainer_prepare.add_argument("--collection", required=True, help="the name of the collection for train")
+    trainer_prepare.add_argument("--collection", help="the name of the collection for train")
     trainer_prepare.add_argument("--topic_format", default="trec", type=str, help="the topic file format for training")
-    trainer_prepare.add_argument("--save_to_snapshot", default="save", type=str,
-                                 help="used to determine the tag of the snapshotted image after indexing")
-    trainer_prepare.add_argument("--test_split", required=True, type=str,
-                                 help="the subset of topic ids to use for testing")
-    trainer_prepare.add_argument("--validation_split", required=True, type=str,
-                                 help="the subset of topic ids to use for validation")
-    trainer_prepare.add_argument("--model_folder", required=True, type=str,
-                                 help="folder where to store the trained model")
+    trainer_prepare.add_argument("--save_to_snapshot", default="save", type=str, help="used to determine the tag of the snapshotted image after indexing")
+    trainer_prepare.add_argument("--test_split", required=True, type=str, help="the subset of topic ids to use for testing")
+    trainer_prepare.add_argument("--validation_split", required=True, type=str, help="the subset of topic ids to use for validation")
+    trainer_prepare.add_argument("--model_folder", required=True, type=str, help="folder where to store the trained model")
     trainer_prepare.add_argument("--qrels", required=True, type=str, help="the qrels file for model selection")
     trainer_prepare.add_argument("--opts", nargs="+", default="", type=str, help="the args passed to the index script")
     trainer_prepare.add_argument("--version", default="", type=str, help="the version string passed to the init script")
@@ -56,17 +50,14 @@ if __name__ == "__main__":
     parser_search.set_defaults(run=manager.search)
     parser_search.add_argument("--repo", required=True, type=str, help="the image repo (i.e., osirrc2019/anserini)")
     parser_search.add_argument("--tag", default="latest", type=str, help="the image tag (i.e., latest)")
-    parser_search.add_argument("--load_from_snapshot", default="save", type=str,
-                               help="used to determine the tag of the snapshotted image to search from")
+    parser_search.add_argument("--load_from_snapshot", default="save", type=str, help="used to determine the tag of the snapshotted image to search from")
     parser_search.add_argument("--collection", required=True, help="the name of the collection")
     parser_search.add_argument("--topic", required=True, type=str, help="the topic file for search")
     parser_search.add_argument("--topic_format", default="trec", type=str, help="the topic file format for search")
     parser_search.add_argument("--top_k", default=1000, type=int, help="the number of results for top-k retrieval")
-    parser_search.add_argument("--output", required=True, type=str,
-                               help="the output directory for run files on the host")
+    parser_search.add_argument("--output", required=True, type=str, help="the output directory for run files on the host")
     parser_search.add_argument("--qrels", required=True, type=str, help="the qrels file for evaluation")
-    parser_search.add_argument("--test_split", required=False, default="", type=str,
-                               help="the subset of topic ids to use for testing")
+    parser_search.add_argument("--test_split", required=False, default="", type=str, help="the subset of topic ids to use for testing")
     parser_search.add_argument("--opts", nargs="+", default="", type=str, help="the args passed to the search script")
     parser_search.add_argument("--timings", action="store_true", help="enable timing information to be printed")
     parser_search.add_argument("--measures", nargs="+", default=["num_q", "map", "P.30"], type=str, help="the measures for trec_eval")
@@ -76,14 +67,10 @@ if __name__ == "__main__":
     parser_interact.set_defaults(run=manager.interact)
     parser_interact.add_argument("--repo", required=True, type=str, help="the image repo (i.e., osirrc2019/anserini)")
     parser_interact.add_argument("--tag", default="latest", type=str, help="the image tag (i.e., latest)")
-    parser_interact.add_argument("--load_from_snapshot", default="save", type=str,
-                                 help="used to determine the tag of the snapshotted image to interact with")
-    parser_interact.add_argument("--exit_jig", default="false", type=str_to_bool,
-                                 help="whether to exit jig after running container")
-    parser_interact.add_argument("--opts", nargs="+", default="", type=str,
-                                 help="the args passed to the interact script")
+    parser_interact.add_argument("--load_from_snapshot", default="save", type=str, help="used to determine the tag of the snapshotted image to interact with")
+    parser_interact.add_argument("--exit_jig", default="false", type=str_to_bool, help="whether to exit jig after running container")
+    parser_interact.add_argument("--opts", nargs="+", default="", type=str, help="the args passed to the interact script")
 
     # Parse the args
     args = parser.parse_args()
-
     args.run(args)
